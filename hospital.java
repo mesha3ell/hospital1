c5c6d3024406a856209364749edc40b1d366b7cd
mesha3ell <alessama9@gmail.com>
2017-04-30 07:55:18 +0300

    Add files via upload

---
+++ hospital.java
@@ -0,0 +1,34 @@
+import java,util.*;
+public class hospital{
+static Scanner read=new Scanner(System.in);
+public static void main(String[])
+{
+int num; 
+do{
+System.out.println("1)Add a patient");
+System.out.println("2)Find a patient");
+System.out.println("3)List all patients");
+System.out.println("4)List patient for a given age");
+System.out.println("5)Number of patients for a given bloodType");
+System.out.println("6)Total number of patients");
+System.out.println("7)Exit");
+num=raed.nextInt();
+
+switch(num){
+case 1:
+
+
+
+}//end switch
+
+
+
+
+
+}while(num!=7);
+
+
+
+
+}
+}
